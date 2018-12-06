3b3a6552210eab110e86581ab9f81cb30d158abb
alexgimson86 <alexgimson@gmail.com>
2018-12-06 17:45:00 -0600

    added post method to add new customers

--- back-end/server.js
+++ back-end/server.js
@@ -6,8 +6,10 @@ const app = express();
 var mongoose = require("mongoose");
 
 const port = 3000;
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.text())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.static('../front-end'))
@@ -19,12 +21,8 @@ var debtSchema = new mongoose.Schema({
    });
 
 var client_debt = mongoose.model("client_debt", debtSchema);
-var debt_doc = new client_debt({name: 'alex gimson', amount_owed: '$30'});
 
-debt_doc.save(function(err,doc){
-    if(err)
-        console.log(err);
-})
+
 
 app.get('/', (req, res) => {
     client_debt.find(function(err,docs){
@@ -33,10 +31,18 @@ app.get('/', (req, res) => {
         }
         res.json({docs})
     })
-
-
-
-
 })
-
+app.post('/', (req, res) => {
+    if(req.body){
+        var name = req.body.customer
+        var debt = req.body.debt
+        var person = {name: name, debt: debt}
+        exports.person = person
+        var newDoc = new client_debt({name: name, amount_owed: debt})
+        newDoc.save()
+        res.status(200).send()
+    }
+    else
+        res.status(404).json({'message': 'error'})
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- front-end/script.js
+++ front-end/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     var link = 'http://localhost:3000';
-    
     (function(){
         $.ajax({
             url: link,
@@ -8,11 +7,36 @@ $(document).ready(function(){
             dataType:"json"
         })
         .done(function(data){
-            $('#greeting').text(data.message)
+            if(data){
+                data.docs.forEach(function(customer){
+                    $('table').append('<tr><td>' + customer.name + '</td>' + '<td>' + customer.amount_owed + '</td></tr>') ;
+                })
+            }
         })
         .fail(function(err){
-
         });
     }());
-
+    //insert new document
+    $('#newDoc').click(function(){
+        if($("input[name='customer']").val() !== '' && $("input[name='debt']").val() !== ''){
+            var customer = $("input[name='customer']").val();
+            var debt = $("input[name='debt']").val()
+            $.ajax({
+                url: link,
+                type: "POST",
+                data: {customer: customer, debt: debt},
+                dataType: JSON
+            })
+            .done(function(data){
+                if(data){
+                    data.docs.forEach(function(doc){
+                        $('table').append('<tr><td>' + customer + '</td>' + '<td>' + debt + '</td></tr>') ;
+                    })
+                }
+            })
+            .fail(function(err){
+                console.log(err)
+            });
+        }
+    })
 });
